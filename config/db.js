2188838a073bebec60f643057ab4d633e80e0317
nzandeg <itk.zandegiacomo@extaccount.com>
2024-07-03 09:07:34 +0200

    deprec

--- config/db.js
+++ config/db.js
@@ -4,16 +4,11 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(
-            process.env.MONGODB_URI,
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true
-            }
-        );
+        console.log('Attempting to connect to MongoDB with URI:', process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log('MongoDB connected to Atlas...');
     } catch (err) {
-        console.error(err.message);
+        console.error('Error connecting to MongoDB:', err.message);
         process.exit(1);
     }
 };
