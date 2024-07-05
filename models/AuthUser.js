ccd0a2b142e07f0592ff1b742ac65997b0faa85e
nzandeg <itk.zandegiacomo@extaccount.com>
2024-07-05 18:27:40 +0200

    login

---
+++ models/AuthUser.js
@@ -0,0 +1,9 @@
+// server/models/AuthUser.js
+const mongoose = require('mongoose');
+
+const authUserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+});
+
+module.exports = mongoose.model('AuthUser', authUserSchema);
