6eb753d7594fcb7e83f10eb85c96c48dc9144e7b
Nicola Zandegiacomo <nicola.zandegiacomo@flyingbasket.com>
2025-01-01 22:55:58 +0100

    lll

--- models/Lettura.js
+++ models/Lettura.js
@@ -3,12 +3,12 @@ const Schema = mongoose.Schema;
 
 const letturaSchema = new Schema(
     {
-        id_lettura: { type: String }, //ridondante
-        data_lettura: { type: Date, required: true },
-        unita_misura: { type: String, required: true },
-        consumo: { type: Number, required: true },
+        id_lettura: { type: String, required: false }, //ridondante
+        data_lettura: { type: Date, required: false },
+        unita_misura: { type: String, required: false },
+        consumo: { type: Number, required: false },
         fatturata: { type: Boolean, default: false },
-        tipo: { type: String, required: true },
+        tipo: { type: String, required: false },
         note: { type: String, required: false },
         contatore: { type: Schema.Types.ObjectId, ref: 'Contatore' }
     },
